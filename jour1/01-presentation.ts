af58c3f85ad38c40aebf09f7b68b70dcc4b0b1e4
malik <malik.h@webdevpro.net>
2020-12-14 09:41:07 +0100

    fin présentation

---
+++ jour1/01-presentation.ts
@@ -0,0 +1,76 @@
+// 01-presentation.ts 
+
+// le typescript => créer d'un ingénieur de chez Microsoft
+
+// Anders Hejlsberg => génie de l'informatique 
+
+// en 2012 => il y a moins de 10 ans 
+
+// langage spécialisé => sur ensemble de javascript 
+
+// SASS => CSS margin / padding ... 
+// @for @mixin .... 
+
+// typescript comprend l'ensemble des mots clés de javascript 
+
+/**
+ * var 
+ * function
+ * number 
+ * string 
+ * ... 
+ * tout ce que vous allez écrire en javascript de base va être comprise 
+ * 
+ * mais typescript ajoute de nouveaux comptes au javascript de base 
+ * => sur set / sur ensemble 
+ */
+
+ /*
+ syntaxent qui n'existent pas en javascript de base 
+
+ 1 introduire => typage le fait de dire de manière explicite la nature d'une variable 
+    lorsque l'on type une variable => réduire les erreurs potentielles lors de l'exécution du code 
+
+ 2 class => vous avez la même chose mais en js 
+ public 
+ private 
+ protected 
+ télécharger du code tiers => ça peut modifier votre code sans lever aucun message d'erreur 
+
+ abstraite / interface ... (JAVA / PHP)
+    éviter de chercher les erreurs avant l'exécution => code plus robuste et plus sûr 
+
+ 3 ES3 ES5 ES6 .... 
+ javacript a des version en fonction de l'age de votre navigateur 
+ votre navigateur va connaitre plus ou moins de nouveau mot clés du langage javascript 
+
+ fonction fléché
+ () => { }
+
+ function nom (){ } // classique de créer une fonction 
+
+ ES6 (2015 de javascript) notation plus courte 
+ let nom = () => { }
+ => en fonction de la manière dont vous écrivez du javascript avec les nouvelles notations 
+ => problème de compatibilité de votre code 
+ => typescript permet d'assurer les rétro compabilité 
+
+ .TS (qui contient du js en version ES6) => .JS qui contient du js en version ES3
+ compiler 
+ transpiler => traduire d'une langage vers un autre ts version 6 => js version 3
+ 
+ */ 
+
+ // créer des fichiers .ts => transformer en fichier .js 
+ // transpiler 
+ // télécharger un logiciel qui permet de réaliser cette transformation 
+// logiciel en ligne de commande 
+// git => git init Enter
+// git add * => 
+// utiliser le terminal 
+// terminal intégré dans visual studio 
+// nodejs ??? 
+// il faut au préalable installer un logiciel nodejs 
+
+// 
+
