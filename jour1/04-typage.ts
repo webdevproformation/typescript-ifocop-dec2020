e94e014d807d7dbe7ac4e24b554abed1c0011663
malik <malik.h@webdevpro.net>
2020-12-14 11:33:21 +0100

    exo

--- jour1/04-typage.ts
+++ jour1/04-typage.ts
@@ -121,3 +121,24 @@ let s : Array< EtudiantInterface > = [  // 4ème possibilité
     { id : 1 , nom : "Alain"  },
     { id : 2 , nom : "Béatrice "}
 ];
+
+// cas pratique : créer un nouveau fichier 05-exo.ts 
+/*
+ce fichier contient les variables suivantes :
+
+/ veuillez les typer les variables suivantes correctement :
+
+// liste1 = "pêche poire ananas";
+// liste2 = ["pêche" , "poire", "ananas"];
+// liste3 = ["pêche", 4, "poire", 3, "ananas" , 2];
+/* liste4 = {
+    items : ["pêche", 4, "poire", 3, "ananas" , 2],
+    budget : 40 ,
+    unite : "euros"
+};*/
+/* liste5 = [
+    {id: 1 , nom : "pêche" , quantite : 4},
+    {id: 2 , nom : "poire" , quantite : 3},
+    {id: 3 , nom : "ananas" , quantite : 2},
+];*/
+*/
