9a00c4499183645494ea2707f968517368095bb5
malik <malik.h@webdevpro.net>
2020-12-15 16:23:28 +0100

    exo

--- jour2/12-module.ts
+++ jour2/12-module.ts
@@ -8,3 +8,27 @@ console.log(informationRectangle( 10 , 12));
 // transformer ts => js
 // pas besoin de préciser les fichiers à importer 
 // tsc 12-module.ts && node 12-module.js
+
+// créer le fichier 13-exo.ts et le fichier 13-exo-lib.ts 
+
+// dans le fichier 13-exo-lib.ts 
+// créer les variables suivantes : 
+// class Salutation{
+/*
+    un attribut data qui contient un tableau avec les informations suivantes 
+    Bonjour Hello Hola
+
+    méthode random 
+     retourner au hasard une des valeurs qui est stockée dans l'attribut data 
+
+    exporte par défaut cette class
+
+    dans le ficheir 13-exo.ts 
+
+    importer la class Salutation et l'initialiser 
+    utiliser la méthode random 4 fois et afficher le résultat dans la console du terminal
+
+    transformer le fichier .ts en .js
+
+*/ 
+//}
